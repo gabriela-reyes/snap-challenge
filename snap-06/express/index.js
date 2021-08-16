7d7365933e6285193a03a4ad0fdef1b009a904f1
gabriela-reyes <86327609+gabriela-reyes@users.noreply.github.com>
2021-08-16 10:09:16 -0600

    updating snap-challenge

--- snap-06/express/index.js
+++ snap-06/express/index.js
@@ -8,4 +8,22 @@ app.use(express.json())
 
 // Add your code here.
 
+//Define our router from Express object
+const indexRoute = express.Router()
+
+const middleware1 = function (request, response, nextFunction) {
+    console.log('hello world')
+    nextFunction()
+}
+
+const middleware2 = (request, response, nextFunction) => {
+    return response.json({status:200, data: null, message: 'this thing is on'})
+}
+
+//Set our router to respond to /apis path
+indexRoute.route('/apis').get(middleware1, middleware2)
+
+//Register route to be used by express
+app.use(indexRoute)
+
 app.listen(4200, () => {console.log("Express has Started")})
